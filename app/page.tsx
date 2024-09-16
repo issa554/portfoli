36ce585934c9ca8df234c75996d6225f7d42b126
issa AbuAwada <67484019+issa554@users.noreply.github.com>
2024-09-16 20:12:34 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from "./components/Hero"
 import Experience from "./components/Experience"
 import Project from "./components/Project"
-
+import { Analytics } from "@vercel/analytics/react"
 export default function Home() {
   return (
  <div>
  <Hero />
  <Experience />
  <Project />
+   <Analytics />
  </div>
   );
 }
